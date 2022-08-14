418bf2ab8b007127fbb544a7fb4b6b68215e7e3f
Polpy <emailparaoedu@gmail.com>
2022-08-14 17:13:23 +0100

    bug fix room creator in your matches

--- src/multiplayer.js
+++ src/multiplayer.js
@@ -386,6 +386,7 @@ export default function Mult() {
                                                             setSideBet(sidebetstr)
                                                             setBetAmmount(nearbetstr)
                                                             setRoomID(roomId)
+                                                            setRoomCreator(window.accountId)
                                                         }
                                                     } />
                                                 </Popup>
