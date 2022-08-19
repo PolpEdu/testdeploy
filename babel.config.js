a3aec6e63b5b5b8b2b0bcebeec6b624f8332feac
Polpy <emailparaoedu@gmail.com>
2022-08-20 00:16:09 +0100

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -2,8 +2,8 @@ module.exports = {
   // presets: ["@babel/env", "babel/react", "@babel/reset-react"],
   plugins: [
     // "@babel/plugin-transform-runtime",
-    "babel/plugin-transform-async-to-generator",
-    "babel/transform-arrow-functions",
+    "@babel/plugin-transform-async-to-generator",
+    "@babel/transform-arrow-functions",
     "@babel/proposal-object-rest-spread",
     "@babel/proposal-class-properties",
   ],
